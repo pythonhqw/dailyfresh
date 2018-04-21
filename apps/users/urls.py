87908b94233a5b55a08cad8b9db9fa3e7eae0a33
pythonhqw <897357806@qq.com>
2018-04-21 17:04:50 +0800

    day02.类视图的配置

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url, include
 from apps.users import views
 
 urlpatterns = [
-    url(r'^register$', views.register, name="register"),
-    url(r'^do_register$', views.do_register, name="do_register"),
+    # 视图函数
+    # url(r'^register$', views.register, name="register"),
+    # url(r'^do_register$', views.do_register, name="do_register"),
+
+    # 类视图        as_view()会返回一个视图函数
+    url(r'^register$', views.RegisterView.as_view(), name="register"),
 
 ]
